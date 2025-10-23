670b005033302868c8a85bbb5a8c43f255262e78
Betina9 <ob042025@gmail.com>
2025-10-23 12:50:24 +0200

    hentet all html inn til js

---
+++ app.js
@@ -0,0 +1,6 @@
+const convertBtn = document.getElementById("convert-btn");
+const inputEl = document.getElementById("unit-input");
+
+const lengthResult = document.getElementById("Length-result");
+const volumResult = document.getElementById("volum-result");
+const massresult = document.getElementById("mass-result");
