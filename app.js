22c9f5f3882f8f59c5e3eb626a2194bb192db21b
Betina9 <ob042025@gmail.com>
2025-10-23 13:25:08 +0200

    Knappen viser nå resultatet

--- app.js
+++ app.js
@@ -1,6 +1,49 @@
 const convertBtn = document.getElementById("convert-btn");
 const inputEl = document.getElementById("unit-input");
 
-const lengthResult = document.getElementById("Length-result");
+const lengthResult = document.getElementById("length-result");
 const volumResult = document.getElementById("volum-result");
-const massresult = document.getElementById("mass-result");
+const massResult = document.getElementById("mass-result");
+
+// Når brukeren klikker på knappen "Convert":
+convertBtn.addEventListener("click", function () {
+  const value = Number(inputEl.value);
+
+  if (!value) {
+    alert("Please enter a number");
+    return;
+  }
+
+  // Konverteringsfaktorer
+  const meterToFeet = 3.281;
+  const literToGallon = 0.264;
+  const kiloToPound = 2.204;
+
+  //Beregninger
+  const metersToFeet = value * meterToFeet;
+  const feetToMeters = value / meterToFeet;
+
+  const literToGallons = value * literToGallon;
+  const gallonsToLiters = value / literToGallon;
+
+  const kilosToPounds = value * kiloToPound;
+  const poundsToKilos = value / kiloToPound;
+
+  // Funksjon som runder ned til 3 desimaler:
+  function roundDown(num) {
+    return Math.floor(num * 1000) / 1000;
+  }
+
+  // Oppdaterer HTML med resultatene:
+  lengthResult.textContent = `${value} meters = ${roundDown(
+    metersToFeet
+  )} feet | ${value} feet = ${roundDown(feetToMeters)} meters`;
+
+  volumResult.textContent = `${value} liters = ${roundDown(
+    literToGallons
+  )} gallons | ${value} gallons = ${roundDown(gallonsToLiters)} liters`;
+
+  massResult.textContent = `${value} kilos = ${roundDown(
+    kilosToPounds
+  )} pounds | ${value} pounds = ${roundDown(poundsToKilos)} kilos`;
+});
